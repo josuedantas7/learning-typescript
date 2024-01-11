c277de938a9db330a0bfa2ba9e852a8da53c2a23
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-11 20:31:59 -0300

    add newRoute

--- ecommerce/src/Routing.tsx
+++ ecommerce/src/Routing.tsx
@@ -6,6 +6,7 @@ import Home from "./App";
 import Product from "./pages/Product";
 import Header from "./components/Header";
 import Container from './components/Container'
+import Carrinho from "./pages/Carrinho";
 
 function App(){
     return(
@@ -16,6 +17,7 @@ function App(){
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/produto/:id" element={<Product />} />
+                        <Route path="/carrinho" element={<Carrinho/>} />
                     </Routes>
                 </Container>
             </Router>
