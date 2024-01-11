20ff139766c41a1f9c0dcb4f751efb7141ce1ea9
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-11 20:32:19 -0300

    hotfix: CardProduct

--- ecommerce/src/components/CardProduct/CardProduct.tsx
+++ ecommerce/src/components/CardProduct/CardProduct.tsx
@@ -6,35 +6,43 @@ import { Link } from 'react-router-dom';
 import { Product } from '../../types/interfaces'
 
 
-const CardProduct = ( {product} : Product ) => {
-
-    const { addCart } = useContext(CartContext)
-
+const CardProduct = ({ product }: { product: Product }) => {
+    const { addCart } = useContext(CartContext);
+  
     function formatNumber(value: number) {
-        return new Intl.NumberFormat("pt-BR", {
-          style: "currency",
-          currency: "BRL",
-        }).format(value);
+      return new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(value);
     }
 
+    function addCartFunction(){
+        if (addCart) {
+            addCart(product);
+        }
+    }
+  
     useEffect(() => {
-        console.log(product)
-    })
-
-  return (
-    <Link to={`produto/${product.id}`} className='w-[250px] h-[350px] relative hover:scale-105 duration-300'>
-        <img className='w-[200px] hover:scale-110 duration-300 h-[250px]' src={product.cover}/>
+      console.log(product);
+    }, [product]);
+  
+    return (
+      <div className='w-[250px] h-[350px] relative hover:scale-105 duration-300'>
+        <img className='w-[200px] hover:scale-110 duration-300 h-[250px]' src={product.cover} alt={product.title} />
         <div className='flex flex-col gap-2'>
-            <h1 className='mt-2'>
-                {product.title}
-            </h1>
-            <div className='flex items-center absolute bottom-0 gap-2'>
-                <p>{formatNumber(product.price)}</p>
-                <button onClick={() => addCart(product)}><IoCartOutline className='rounded-md bg-black text-white p-1 text-[25px]' /></button>
-            </div>
+          <Link to={`produto/${product.id}`} className='mt-2'>
+            {product.title}
+          </Link>
+          <div className='flex items-center absolute bottom-0 gap-2'>
+            <p>{formatNumber(product.price)}</p>
+            <button onClick={() => addCartFunction()}>
+              <IoCartOutline className='rounded-md bg-black text-white p-1 text-[25px]' />
+            </button>
+          </div>
         </div>
-    </Link>
-  )
-}
-
-export default CardProduct
+      </div>
+    );
+  };
+  
+  export default CardProduct;
+  
--- ecommerce/src/components/Header.tsx
+++ ecommerce/src/components/Header.tsx
@@ -7,20 +7,20 @@ import { Link } from 'react-router-dom';
 const Header = () => {
 
     const { cart } = useContext(CartContext)
-    const [qtd, setQtd] = useState(cart.length)
+    const [qtd, setQtd] = useState<number | undefined>()
 
     useEffect(() => {
-        setQtd(cart.length)
+        setQtd(cart?.length)
     },[cart])
 
   return (
     <div className='w-full py-2 bg-gray-300'>
         <div className='w-4/5 flex justify-between mx-auto'>
             <Link to={'/'} className='hover:scale-110 duration-500 text-3xl font-bold'>Dev Shop</Link>
-            <div className='relative cursor-pointer hover:scale-110 duration-300 py-1 pr-2'>
+            <Link to={"/carrinho"} className='relative cursor-pointer hover:scale-110 duration-300 py-1 pr-2'>
                 <IoCartOutline className='text-[40px] duration-500' />
                 <p className='absolute z-20 bg-blue-300 rounded-full px-2 top-0 right-0'>{qtd !== 0 && qtd}</p>
-            </div>
+            </Link>
         </div>
     </div>
   )
