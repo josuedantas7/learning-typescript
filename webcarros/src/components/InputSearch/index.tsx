8fcdb01f8b4bae5d65efbfd57642fc9e90f7f994
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-15 13:56:16 -0300

    feat: inputSearch

---
+++ webcarros/src/components/InputSearch/index.tsx
@@ -0,0 +1,23 @@
+import { ChangeEvent, SetStateAction } from "react";
+
+interface InputSearchProps {
+    setSearch: React.Dispatch<SetStateAction<string>>;
+}
+  
+const InputSearch = ({setSearch} : InputSearchProps) => {
+    
+    const handleChange = (e : ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+    };
+
+  return (
+    <div className='flex justify-between gap-5 rounded-xl w-[90%] mx-auto bg-white p-4'>
+        <div className='border-2 relative w-4/5 rounded-md'>
+            <input onChange={handleChange} placeholder='Digite o nome do carro...' type="text" className='w-full rounded-md h-full py-2 px-5' />
+        </div>
+        <button className='w-1/5 bg-red-500 text-white font-bold rounded-md'>Buscar</button>
+    </div>
+  )
+}
+
+export default InputSearch
