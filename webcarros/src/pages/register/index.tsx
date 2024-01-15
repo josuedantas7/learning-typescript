0e16e48a1426d91c3ea7a83ef9529730a2edfda9
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-15 18:13:47 -0300

    feat: page Cadastro

--- webcarros/src/pages/register/index.tsx
+++ webcarros/src/pages/register/index.tsx
@@ -1,16 +1,61 @@
+import { useEffect, useState } from 'react'
 import logo from '../../assets/logo.svg'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { auth } from '../../services/firebaseConnection'
+import { createUserWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth'
+
+interface UserProps{
+  name: string;
+  email: string;
+  password: string;
+}
 
 const Register = () => {
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const navigate = useNavigate()
+
+
+  async function onSubmit(e : React.MouseEvent<HTMLButtonElement, MouseEvent>){
+    e.preventDefault()
+    const data : UserProps = {
+      name,
+      email,
+      password
+    }
+    createUserWithEmailAndPassword(auth, data.email, data.password)
+    .then( async (user) => {
+      await updateProfile(user.user, {
+        displayName: data.name
+      })
+
+      alert('Usuário cadastrado com sucesso!')
+      navigate('/dashboard', { replace: true })
+    }).catch((err) => {
+      console.log(err)
+      alert('Algo deu errado, tente novamente!')
+    })
+  }
+
+  useEffect(() => {
+    async function handleLogout(){
+      await signOut(auth)
+    }
+    handleLogout()
+  },[])
+
   return (
     <div className='h-[92vh] w-full flex flex-col items-center justify-center mx-auto'>
       <div className='w-1/2 max-[1000px]:w-[80%] max-[590px]:w-[90%] max-[520px]:w-[100%]'>
         <img src={logo} className='w-[300px] mx-auto h-[100px] mb-8'/>
         <div className='flex flex-col bg-white gap-4 p-4 rounded-lg'>
-          <input className='border-2 rounded-md px-4 py-2' placeholder='Digite seu nome completo...' type="text" />
-          <input className='border-2 rounded-md px-4 py-2' placeholder='Digite o email...' type="text" />
-          <input className='border-2 rounded-md px-4 py-2' placeholder='Digite a senha...' type="text" />
-          <button className='bg-black text-white font-bold py-2 rounded-md hover:bg-gray-800'>Cadastrar</button>
+          <input onChange={(e) => setName(e.target.value)} className='border-2 rounded-md px-4 py-2' placeholder='Digite seu nome completo...' type="text" />
+          <input onChange={(e) => setEmail(e.target.value)} className='border-2 rounded-md px-4 py-2' placeholder='Digite o email...' type="email" />
+          <input onChange={(e) => setPassword(e.target.value)} className='border-2 rounded-md px-4 py-2' placeholder='Digite a senha...' type="password" />
+          <button onClick={(e) => onSubmit(e)} className='bg-black text-white font-bold py-2 rounded-md hover:bg-gray-800'>Cadastrar</button>
         </div>
         <div className='flex justify-center gap-2'>
           <span>Já possui uma conta?</span>
