3178a9e3da9bfd56cb613143206628d513d5d66d
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-15 18:13:28 -0300

    feat: page Login

--- webcarros/src/pages/login/index.tsx
+++ webcarros/src/pages/login/index.tsx
@@ -1,15 +1,55 @@
+import { useState, useEffect } from 'react'
 import logo from '../../assets/logo.svg'
 import { Link } from 'react-router-dom'
 
+import { signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { auth } from '../../services/firebaseConnection'
+
+import { useNavigate } from 'react-router-dom'
+
+interface UserProps{
+  email: string;
+  password: string;
+}
+
 const Login = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const navigate = useNavigate()
+  function onSubmit(e : React.MouseEvent<HTMLButtonElement, MouseEvent>){
+    e.preventDefault()
+
+    const data : UserProps = {
+      email,
+      password
+    }
+
+    signInWithEmailAndPassword(auth, data.email, data.password)
+    .then((user) => {
+      alert('Usuário logado com sucesso!')
+      navigate('/dashboard', { replace: true })
+      console.log(user)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    async function handleLogout() : Promise<void>{
+      await signOut(auth)
+    }
+    handleLogout()
+  },[])
+
   return (
     <div className='h-[92vh] w-full flex flex-col items-center justify-center mx-auto'>
         <div className='w-1/2 max-[1000px]:w-[80%] max-[590px]:w-[90%] max-[520px]:w-[100%]'>
           <img src={logo} className='w-[300px] mx-auto h-[100px] mb-8'/>
           <div className='flex flex-col bg-white gap-4 p-4 rounded-lg'>
-            <input className='border-2 rounded-md px-4 py-2' placeholder='Digite o email...' type="text" />
-            <input className='border-2 rounded-md px-4 py-2' placeholder='Digite a senha...' type="text" />
-            <button className='bg-black text-white font-bold py-2 rounded-md hover:bg-gray-800'>Acessar</button>
+            <input onChange={(e) => setEmail(e.target.value)} className='border-2 rounded-md px-4 py-2' placeholder='Digite o email...' type="email" />
+            <input onChange={(e) => setPassword(e.target.value)} className='border-2 rounded-md px-4 py-2' placeholder='Digite a senha...' type="password" />
+            <button onClick={(e) => onSubmit(e)} className='bg-black text-white font-bold py-2 rounded-md hover:bg-gray-800'>Acessar</button>
           </div>
           <div className='flex justify-center gap-2'>
             <span>Ainda não possui uma conta?</span>
