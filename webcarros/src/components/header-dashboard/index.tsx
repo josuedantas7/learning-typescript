59f7ea43ca994a03862ddc17de2ab087489c5e95
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-15 18:14:12 -0300

    feat: Header DashBoard

---
+++ webcarros/src/components/header-dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header_Dashboard() {
+  return (
+    <div className='flex bg-red-500 text-white py-1 rounded-lg px-6 font-semibold gap-5 items-start'>
+        <Link to='/dashboard'>Dashboard</Link>
+        <Link to='/dashboard/new'>Novo carro</Link>
+    </div>
+  )
+}
+
+export default Header_Dashboard
