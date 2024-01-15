866c80c83589753ed5403a0ee63cc3a6fd9e0af1
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-15 18:14:50 -0300

    feat: Routing component

--- webcarros/src/App.tsx
+++ webcarros/src/App.tsx
@@ -1,8 +1,36 @@
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Dashboard from "./pages/dashboard";
+import CarDetail from "./pages/car";
+import Home from "./pages/home";
+import New from './pages/dashboard/new'
+
+import Header from "./components/header";
+import Container from "./components/Container";
+
+import AuthProvider from "./contexts/AuthContext";
+
+
+const App = () => {
   return (
-    <>
-      <h1 className="text-blue-900 text-3xl">Web Carros</h1>
-    </>
-  )
-}
+    <Router>
+      <AuthProvider>
+        <Header />
+        <Container>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Register />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/dashboard/new" element={<New />} />
+              <Route path="/car/:id" element={<CarDetail />} />
+          </Routes>
+        </Container>
+      </AuthProvider>
+    </Router>
+  );
+};
+
+export default App;
