f1ae5f29e527949cfad16989deb014728c578dea
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-18 08:52:00 -0300

    hotfix using the state signed on header

--- webcarros/src/components/header/index.tsx
+++ webcarros/src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 
 import logo from '../../assets/logo.svg'
 
@@ -15,18 +15,9 @@ const Header = () => {
 
   const { signed } = useContext(AuthContext)
 
-  const [authenticated,setAuthenticated] = useState<boolean>(false)
-
   async function handleLogout(): Promise<void>{
     await signOut(auth)
-    setAuthenticated(false)
   }
-
-  useEffect(() => {
-    if (signed) {
-      setAuthenticated(true)
-    }
-  },[signed])
 
   return (
     <div className='px-4 py-3 border-b-[1px] border-black flex justify-between'>
@@ -34,7 +25,7 @@ const Header = () => {
           <img src={logo} />
         </Link>
         <ThemeSwitcher />
-        {authenticated ? <MdLogout onClick={handleLogout}  className='text-3xl border-[1px] border-black rounded-full w-[40px] h-[40px] p-2'/> : (
+        {signed ? <MdLogout onClick={handleLogout}  className='text-3xl border-[1px] border-black rounded-full w-[40px] h-[40px] p-2'/> : (
           <Link to="/login"><RxPerson className='text-3xl border-[1px] border-black rounded-full w-[40px] h-[40px] p-2' /></Link>
         )}
     </div>
