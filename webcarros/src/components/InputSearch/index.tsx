bf1a60083c03196b24aba3185d12538e47358883
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-18 08:55:57 -0300

    feat: inputSearch

--- webcarros/src/components/InputSearch/index.tsx
+++ webcarros/src/components/InputSearch/index.tsx
@@ -1,10 +1,12 @@
 import { ChangeEvent, SetStateAction } from "react";
 
 interface InputSearchProps {
+    input: string;
     setSearch: React.Dispatch<SetStateAction<string>>;
+    handleSearchCar: () => void;
 }
   
-const InputSearch = ({setSearch} : InputSearchProps) => {
+const InputSearch = ({input,setSearch, handleSearchCar} : InputSearchProps) => {
     
     const handleChange = (e : ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
@@ -13,9 +15,9 @@ const InputSearch = ({setSearch} : InputSearchProps) => {
   return (
     <div className='flex justify-between gap-5 rounded-xl w-[90%] mx-auto bg-white p-4'>
         <div className='border-2 relative w-4/5 rounded-md'>
-            <input onChange={handleChange} placeholder='Digite o nome do carro...' type="text" className='w-full rounded-md h-full py-2 px-5' />
+            <input value={input} onChange={handleChange} placeholder='Digite o nome do carro...' type="text" className='w-full rounded-md h-full py-2 px-5' />
         </div>
-        <button className='w-1/5 bg-red-500 text-white font-bold rounded-md'>Buscar</button>
+        <button onClick={() => handleSearchCar()} className='w-1/5 bg-red-500 text-white font-bold rounded-md'>Buscar</button>
     </div>
   )
 }
