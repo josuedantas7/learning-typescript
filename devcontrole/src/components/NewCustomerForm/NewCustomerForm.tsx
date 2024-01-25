5925f55315cff77c2c914965e84be7cb127bde4e
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-25 12:21:58 -0300

    feat: component create new Customer

---
+++ devcontrole/src/components/NewCustomerForm/NewCustomerForm.tsx
@@ -0,0 +1,88 @@
+'use client'
+import React from 'react'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+
+import { useForm } from 'react-hook-form'
+
+import { z } from 'zod'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { api } from '@/lib/api'
+
+type CreateuserFormData = z.infer<typeof createUserFormSchema>
+
+interface UserProps{
+    name: string;
+    tel: string;
+    email: string;
+    address?: string;
+}
+
+const createUserFormSchema = z.object({
+    name: z.string().min(1, 'Campo nome não pode ser vazio').transform(name => {
+        return name.trim().split(' ').map(word => word[0].toUpperCase().concat(word.slice(1))).join(' ')
+    }),
+    email: z.string().email('Email inválido').min(1, 'Campo email não pode ser vazio'),
+    tel: z.string().refine((value) => {
+      return /^(?:\(\d{2}\)\s?)?\d{9}$/.test(value) || /^\d{2}\s\d{9}$/.test(value) || /^\d{11}$/.test(value)
+    }, {
+      message: "O numero de telefone deve estar (DD) 999999999"
+    }),
+    address: z.string()
+})
+
+const NewCustomerForm = ( {userId}:  { userId : string}) => {
+
+    const { 
+        register, 
+        handleSubmit, 
+        formState: { errors } } = useForm<CreateuserFormData>({
+        resolver: zodResolver(createUserFormSchema)
+      })
+
+
+    async function onSubmit(data : UserProps){
+        const response = await api.post('/api/customer', {
+            name: data.name,
+            phone: data.tel,
+            email: data.email,
+            address: data.address,
+            userId: userId
+        })
+        console.log(response.data)
+    }
+
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className='w-full flex flex-col gap-4 mt-8'>
+        <div className="w-full items-center gap-1.5">
+            <Label htmlFor="name">Nome completo</Label>
+            <Input {...register('name')} type="text" id="name" placeholder="Digite o nome..." />
+            {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+        </div>
+        <div className='flex gap-3'>
+            <div className="w-full items-center gap-1.5">
+                <Label htmlFor="tel">Telefone</Label>
+                <Input {...register('tel')} type="text" id="tel" placeholder="Exemplo: (94) 984231330" />
+                {errors.tel && <p className='text-red-500'>{errors.tel.message}</p>}
+            </div>
+            <div className="w-full items-center gap-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input {...register('email')} type="email" id="email" placeholder="Email" />
+                {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+            </div>
+        </div>
+        <div className="w-full items-center gap-1.5">
+            <Label htmlFor="endereco">Endereço</Label>
+            <Input {...register('address')} type="text" id="endereco" placeholder="Digite o endereço do cliente" />
+            {errors.address && <p className='text-red-500'>{errors.address.message}</p>}
+        </div>
+        <Button type='submit' className="w-full mt-4">Cadastrar cliente</Button>
+    </form>
+  )
+}
+
+export default NewCustomerForm
