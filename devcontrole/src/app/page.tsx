791059fd66ea69959188fe7ac71ba4de5f67fccf
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-28 19:28:42 -0300

    feat: homepage

--- devcontrole/src/app/page.tsx
+++ devcontrole/src/app/page.tsx
@@ -1,7 +1,12 @@
+import imageHome from '@/assets/home.svg'
+import Image from 'next/image';
+import Link from 'next/link';
 export default function Home() {
   return (
-    <main>
-      <h1>Home page</h1>
+    <main className='w-full flex justify-center mt-8 max-[420px]:mt-0'>
+      <Link className='w-full mx-auto' href={'/login'}>
+        <Image alt='Imagem Home' className='w-[600px] mx-auto h-[550px] max-[720px]:w-[90%] max-[720px]:h-[500px]' src={imageHome} />
+      </Link>
     </main>
   );
 }
